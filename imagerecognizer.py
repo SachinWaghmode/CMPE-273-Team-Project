101f21499a7b4501e0db0ad2199c14de55360478
ghotanesg <saurabhghotane@Saurabhs-MacBook-Pro.local>
2017-05-04 03:56:02 -0700

    Removed the hardcoding of source  paths.

--- imagerecognizer.py
+++ imagerecognizer.py
@@ -1,29 +1,37 @@
 import os
 import cv2
 import numpy as np
-from PIL import Image, ImagePath
+from PIL import Image
 
 recognizer = cv2.face.createLBPHFaceRecognizer()
-path = '/home/manish/PycharmProjects/ImageSearch/dataset'
 
+path = 'dataset/'
 
 def getImagesWithID(path):
+
+    # Get Path of each Image in the data set folder
     imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
-    print imagePaths
+
+    #print imagePaths
     faces = []
+
     IDs=[]
+
     for imagePath in imagePaths:
         faceImg=Image.open(imagePath).convert('L')
+
         faceNp=np.array(faceImg,'uint8')
+
         ID= int(os.path.split(imagePath)[-1].split('.')[1])
+
         faces.append(faceNp)
-        print ID
+
         IDs.append(ID)
         cv2.imshow("training",faceNp)
         cv2.waitKey(10)
     return np.array(IDs), faces
 
-Ids,faces = getImagesWithID(path)
+Ids, faces = getImagesWithID(path)
 recognizer.train(faces,Ids)
 recognizer.save('recognizer/trainingData.yml')
 cv2.destroyAllWindows()
